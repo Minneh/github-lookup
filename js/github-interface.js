51178855056928aa7949f2f58438474709d3e0e3
Minneh Mugo <minneh.mugo@gmail.com>
2017-03-22 14:17:06 +0300

    edit clearrepositories button and include emptying of repos onClick

--- js/github-interface.js
+++ js/github-interface.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     $("#username").val("");
     $("ol#list").innerHTML = "";
      $(".showUser").text(user);
+     removeAll();
      $.get('https://api.github.com/users/' + user + '/repos?access_token='+ apiKey, function(response){
         response.forEach(function (repo) {
           $("ol#list").append("<li>" + repo.name + "</li>");
